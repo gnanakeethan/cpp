a9bac2cfdf12d40500c003c3c979c07c9861f3fb
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2017-08-16 15:44:32 +0530

    Update ex5.cpp

--- 2017-08-10 Data Types/ex5.cpp
+++ 2017-08-10 Data Types/ex5.cpp
@@ -12,13 +12,13 @@ REPEAT: {
         cin >> seconds;
 
         int days = seconds/86400;
-        seconds = seconds%86400;
+        seconds %=86400;
 
         int hours = seconds/3600;
-        seconds=  seconds%3600;
+        seconds%=3600;
 
         int minutes = seconds/60;
-        seconds = seconds%60;
+        seconds %=60;
 
         cout << days <<" Days " << hours <<" Hours "<< minutes<< " Minutes "<< seconds<< " Seconds \n";
     };
